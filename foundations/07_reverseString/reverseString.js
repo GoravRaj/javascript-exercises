542e918c4b01c94c2c303620ca12698155a0f529
GoravRAj <goravtriton@gmail.com>
2026-02-09 13:19:05 +0530

    My solution to reversing a string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newarr = [];
+    for(let i = str.length - 1; i >= 0; i--) {
+        newarr.push(str[i]);
+    }
+    return newarr.join("");
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
