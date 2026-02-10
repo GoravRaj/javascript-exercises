f4891ee92a886f691da003cfb36740de60dc42c4
GoravRAj <goravtriton@gmail.com>
2026-02-10 09:22:30 +0530

    Solve removeFromArray exercise
    Implemented array filter using nested loop for multi-argument removal

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toBeRemoved) {
+    return arr.filter((item) => {
+        for(let i = 0; i < toBeRemoved.length; i++) {
+            if(item === toBeRemoved[i]) {
+                return false;
+            } 
+        }  
+        return true;   
+    });
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
