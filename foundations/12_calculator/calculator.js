bd02f25fb2a857777b9759d5b8747ebb8c36e87e
GoravRAj <goravtriton@gmail.com>
2026-03-01 12:49:02 +0530

    Made functions for arithmetic operations
    Some functions include working with an array

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, val) => total + val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, val) => {
+    return total *= val
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return base**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+	for(let i = num; i > 0; i--){
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
