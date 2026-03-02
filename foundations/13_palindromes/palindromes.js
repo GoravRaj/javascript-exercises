171016fb0c425a7739074d1058f3b01d2acf2a9a
GoravRAj <goravtriton@gmail.com>
2026-03-02 08:39:02 +0530

    Created palindrome checker
    using combination of string and array methods
    includes punctuation cleaning

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const validChars = "abcdefghijklmnopqrstuvwxyz1234567890";
+    const cleanStr = str.toLowerCase().split("").filter((char) => {
+      return validChars.includes(char);
+    }).join("");
+    const reversed = cleanStr.split("").reverse().join("");
+    return cleanStr === reversed;
 };
 
 // Do not edit below this line
