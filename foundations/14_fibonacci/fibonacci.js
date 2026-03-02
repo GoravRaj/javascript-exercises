a6e96b06968772435df9cba19a390b9b8f651d94
GoravRAj <goravtriton@gmail.com>
2026-03-02 11:30:55 +0530

    Complete fibonacci exercise and handle edge cases

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const index = Number(num);
+    
+    if(index < 0) {
+        return "OOPS";
+    }else if(index == 0) {
+        return 0;
+    }
 
+    let [prev, current] = [0, 1];
+
+    for(let i = 2; i <= index; i++) {
+        let fiboNum = prev + current;
+        prev = current;
+        current = fiboNum;
+    }
+    
+    return current;
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
