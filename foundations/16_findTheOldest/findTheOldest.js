28aeb1a59d18a5f17054f5f331f07158e8fe4dee
GoravRAj <goravtriton@gmail.com>
2026-03-03 12:28:06 +0530

    implement findTheOldest with dynamic age calculation
    used reduce without initial value to handle oldest person comparison

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const getAge = (birthYear, deathYear) => {
+        return (deathYear || new Date().getFullYear()) - birthYear;
+    }
+
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        return oldestAge < currentAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
